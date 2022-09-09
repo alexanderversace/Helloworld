428c4a456aa1c3fee7067cc8dbca577cfba12867
alexanderversace <113153853+alexanderversace@users.noreply.github.com>
2022-09-08 22:55:32 -0500

    Update Main.java
    Modificado en la web

--- Helloworld.java/src/Main.java
+++ Helloworld.java/src/Main.java
@@ -3,5 +3,9 @@ public class Main {
 
         System.out.println("Hello world!");
         System.out.println("cambio enviado desde intellij");
+      
+        //cambio modificado en la nube!!
+      for(int i=o; i<10; i++){
+          System.out.println("Hola");
     }
 }
